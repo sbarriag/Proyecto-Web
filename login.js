a2db0a3294d1cc034d06064b4ea59502bd983fb1
Sebastián <sbarriag@uc.cl>
2025-05-04 14:38:23 -0400

    Primer commit

---
+++ login.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    // Prevenir el envío del formulario
+    event.preventDefault();
+    console.log("Formulario de inicio de sesión capturado");
+});
---
+++ register.js
@@ -0,0 +1,6 @@
+// Script básico para el formulario de registro
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    // Prevenir el envío del formulario
+    event.preventDefault();
+    console.log("Formulario de registro capturado");
+});
