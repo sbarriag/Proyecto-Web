12c10cda9a8eeaabcb8a59f26e02dad4616cca03
Sebastián <sbarriag@uc.cl>
2025-05-12 11:47:42 -0400

    AAA

--- login.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    // Prevenir el envío del formulario
-    event.preventDefault();
-    console.log("Formulario de inicio de sesión capturado");
-});
--- register.js
+++
@@ -1,6 +0,0 @@
-// Script básico para el formulario de registro
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    // Prevenir el envío del formulario
-    event.preventDefault();
-    console.log("Formulario de registro capturado");
-});
